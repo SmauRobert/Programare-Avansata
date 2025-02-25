caca8fa77723107b422aa998122b21baf9cab05e
Smau Robert <robertgeorgesmau@gmail.com>
2025-02-26 00:49:19 +0200

    Compulsory - Done | Homework - Done | Bonus - Done

--- Lab I/src/Lab1.java
+++ Lab I/src/Lab1.java
@@ -7,7 +7,8 @@ public class Lab1 {
     public static void main(String[] args) {
         Lab1 lab1 = new Lab1();
 //        lab1.compulsory();
-        lab1.homework(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+//        lab1.homework(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        lab1.bonus();
     }
 
     void compulsory() {
@@ -40,6 +41,7 @@ public class Lab1 {
     private void printMatrix(boolean[][] matrix, int n) {
         StringBuilder matrixString = new StringBuilder();
         int cellSize = 2 + getDigitCount(n);
+        cellSize += 1 - cellSize % 2;
 
         matrixString.append("╔").append("═".repeat(cellSize));
         for(int node = 0; node < n; node ++) {
@@ -127,21 +129,26 @@ public class Lab1 {
         System.out.println("The number of edges is " + edgeCount);
 
         int minDegree = edgeCount + 1, maxDegree = -1;
+        int minNode = -1, maxNode = -1;
         for(int node1 = 0; node1 < n; node1 ++) {
             int currentDegree = 0;
             for(int node2 = 0; node2 < n; node2 ++) {
                 currentDegree += AdjacencyMatrix[node1][node2] ? 1 : 0;
             }
 
-            if(currentDegree < minDegree)
+            if(currentDegree < minDegree) {
                 minDegree = currentDegree;
+                minNode = node1;
+            }
 
-            if(currentDegree > maxDegree)
+            if(currentDegree > maxDegree) {
                 maxDegree = currentDegree;
+                maxNode = node1;
+            }
         }
 
-        System.out.println("Δ(G) = " + minDegree);
-        System.out.println("δ(G) = " + maxDegree);
+        System.out.println("δ(G) = " + minDegree + " | Node with minimum degree is " + minNode);
+        System.out.println("Δ(G) = " + maxDegree + " | Node with maximum degree is " + maxNode);
 
         int degreeSum = 0;
         for(int node1 = 0; node1 < n; node1 ++) {
@@ -156,7 +163,96 @@ public class Lab1 {
         System.out.println("Execution time: " + TimeUnit.MILLISECONDS.toSeconds(EndTime - StartTime) + " seconds");
     }
 
-    void bonus() {
+    private boolean[][] GenerateAdjacencyMatrix(int n) {
+        boolean[][] AdjacencyMatrix = new boolean[n][n];
+        for(int node1 = 0; node1 < n; node1 ++) {
+            int degree = (int)(Math.random() * n);
+            for(int i = 0; i < degree; i ++) {
+                int node2 = (int)(Math.random() * n);
+                if(node1 != node2) {
+                    AdjacencyMatrix[node1][node2] = true;
+                    AdjacencyMatrix[node2][node1] = true;
+                }
+            }
+        }
+        return AdjacencyMatrix;
+    }
 
+    private boolean[][] GetTransposedGraph(boolean[][] AdjacencyMatrix, int n) {
+        boolean[][] TransposedMatrix = new boolean[n][n];
+        for(int node1 = 0; node1 < n; node1 ++) {
+            for(int node2 = 0; node2 < n; node2 ++) {
+                TransposedMatrix[node1][node2] = !AdjacencyMatrix[node1][node2];
+            }
+        }
+        return TransposedMatrix;
+    }
+
+    private boolean Backtrack(int index, int[] Picks, boolean[] PicksUsed, boolean[][] AdjacencyMatrix) {
+        if(index == Picks.length) {
+            for(int index1 = 0; index1 < Picks.length; index1 ++) {
+                for(int index2 = index1 + 1; index2 < Picks.length; index2 ++) {
+                    if(!AdjacencyMatrix[Picks[index1]][Picks[index2]]) {
+                        return false;
+                    }
+                }
+            }
+            System.out.println("Picks: " + java.util.Arrays.toString(Picks));
+            return true;
+        }
+
+        if(index == 0) {
+            for(int i = 0; i < PicksUsed.length; i ++)
+                if(!PicksUsed[i]) {
+                    PicksUsed[i] = true;
+                    Picks[0] = i;
+                    if(Backtrack(1, Picks, PicksUsed, AdjacencyMatrix)) {
+                        return true;
+                    }
+                    PicksUsed[i] = false;
+                }
+
+            return false;
+        }
+
+        for(int i = Picks[index - 1]; i < PicksUsed.length; i ++)
+            if(!PicksUsed[i]) {
+                PicksUsed[i] = true;
+                Picks[index] = i;
+                if(Backtrack(index + 1, Picks, PicksUsed, AdjacencyMatrix)) {
+                    return true;
+                }
+                PicksUsed[i] = false;
+            }
+
+        return false;
+    }
+
+    private boolean Clique(boolean[][] AdjacencyMatrix, int n, int k) {
+        int[] Picks = new int[k];
+        boolean[] PicksUsed = new boolean[n];
+
+        return Backtrack(0, Picks, PicksUsed, AdjacencyMatrix);
+    }
+
+    private boolean StableSet(boolean[][] AdjacencyMatrix, int n, int k) {
+        boolean[][] TransposedMatrix = GetTransposedGraph(AdjacencyMatrix, n);
+        return Clique(TransposedMatrix, n, k);
+    }
+
+    void bonus() {
+        int n = (int)(Math.random() * 20);
+        int k = (int)(Math.random() * n);
+        boolean[][] AdjacencyMatrix = GenerateAdjacencyMatrix(n);
+        printMatrix(AdjacencyMatrix, n);
+        System.out.println("N = " + n + " | K = " + k);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Does the graph have a clique of size ≥ " + k + " ? " + Clique(AdjacencyMatrix, n, k));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution time: " + TimeUnit.MILLISECONDS.toSeconds(endTime - startTime) + " seconds");
+        startTime = System.currentTimeMillis();
+        System.out.println("Does the graph have a stable set of size ≥ " + k + " ? " + StableSet(AdjacencyMatrix, n, k));
+        endTime = System.currentTimeMillis();
+        System.out.println("Execution time: " + TimeUnit.MILLISECONDS.toSeconds(endTime - startTime) + " seconds");
     }
 }
